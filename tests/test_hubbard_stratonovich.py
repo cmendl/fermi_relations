fbc02a7e495fbc84c504890b0425e1d6f8750038
Christian B. Mendl <christian.b.mendl@gmail.com>
2025-11-02 21:44:05 +0100

    continuous Hubbard-Stratonovich transformation for a squared free-fermion operator, evaluated via Gauss-Hermite quadrature

--- tests/test_hubbard_stratonovich.py
+++ tests/test_hubbard_stratonovich.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy as np
+from scipy.linalg import expm
 from scipy.stats import unitary_group
 import fermi_relations as fr
 
@@ -59,6 +60,53 @@ class TestHubbardStratonovich(unittest.TestCase):
 
                 # compare
                 self.assertTrue(np.allclose(psi_t, psi_t_ref))
+
+    def test_continuous_hubbard_stratonovich(self):
+        """
+        Test the continuous Hubbard-Stratonovich transformation based on a Gaussian distribution
+        and approximate the integral by Gauss-Hermite quadrature.
+        """
+        rng = np.random.default_rng()
+
+        # number of modes
+        nmodes = 6
+
+        clist, alist, _ = fr.construct_fermionic_operators(nmodes)
+
+        # random single-particle operator (not Hermitian in general)
+        b = 0.5 * fr.crandn((nmodes, nmodes), rng)
+        # free-fermion operator on full Fock space
+        bfull = sum(b[i, j] * (clist[i] @ alist[j])
+                    for i in range(nmodes)
+                    for j in range(nmodes)).toarray()
+
+        # squared operator
+        vfull = bfull @ bfull
+
+        t = 0.3
+
+        # reference time evolution operator
+        ufull_ref = expm(-0.5j * t * vfull)
+
+        # continuous Hubbard-Stratonovich transformation
+        phase = np.exp(-1j * np.pi / 4)
+        points, weights = gauss_hermite_quadrature(15)
+        ufull = sum(w * expm(-phase * x * np.sqrt(t) * bfull) for x, w in zip(points, weights))
+
+        # compare
+        self.assertTrue(np.allclose(ufull, ufull_ref, rtol=1e-13))
+
+
+def gauss_hermite_quadrature(n: int):
+    """
+    Compute the Gauss-Hermite quadrature points and weights.
+    """
+    # Golub-Welsch algorithm
+    entries = np.sqrt(np.arange(1, n))
+    points, vecs = np.linalg.eigh(np.diag(entries, 1) + np.diag(entries, -1))
+    # require that eigenvectors are normalized
+    weights = np.abs(vecs[0, :])**2
+    return points, weights
 
 
 if __name__ == '__main__':
